5d65a976ef112e5c998d822a9bac6de98606ddf5
tom-king-005b64 <36082946+dehnsurgeon@users.noreply.github.com>
2025-06-12 20:46:59 +0100

    changed scale of drops

--- homepage/flow.js
+++ homepage/flow.js
@@ -122,7 +122,7 @@ class Drop {
 }
 
 document.onmousedown = function (e) {
-    const drop = new Drop(e.pageX, e.pageY, width/5, randomRGB())
+    const drop = new Drop(e.pageX, e.pageY, width/20, randomRGB())
 
     for (let other of drops) {
         drop.marble(other);
